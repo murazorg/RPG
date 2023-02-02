95dc9a16b26703fed8a6335a55f8ab87d6a808ca
murazorg <123461932+murazorg@users.noreply.github.com>
2023-02-02 16:07:38 +0300

    Update artefacts.py

--- artefacts.py
+++ artefacts.py
@@ -1,5 +1,6 @@
 class Artefact():
-    artefacts = {'Гномий щит': ['Левая рука', False, False, None], 'Aboba': ['Левая рука', False, False, None]}
+    artefacts = {'Гномий щит': ['Левая рука', False, False, None], 'Кулон смерти': ['Шея', False, False, None],
+                 'Кулон жизни': ['Шея', False, False, None]}
     count = 0
 
     def get_artefact(self, name):
@@ -8,25 +9,45 @@ class Artefact():
         self.artefacts[name][3] = self.count
         print('Предмет {0} подобран в рюкзак'.format(name))
 
-    def wear(self, id):
-        for key, value, in self.artefacts.items():
-            if value[3] == id:
-                if value[2]:
-                    value[2] = False
-                    print('Предмет {0} убран в рюкзак'.format(key))
-                    return True
-                else:
-                    value[2] = True
-                    print('Предмет {0} экипирован'.format(key))
-                    self.effects(key)
-                    return True
+    def wear(self, id, person):
+        for key, value in self.artefacts.items():
+            if value[3] == id and value[2]:  # Он одет
+                value[2] = False  # Снимаем
+                print('Предмет {0} убран в рюкзак'.format(key))
+                self.effect_off(key, person)  # Убираем эффекты
+                return True
+            elif value[3] == id and (value[2] is False):  # Он не одет
+                for k, v in self.artefacts.items():
+                    if v[0] == value[0] and v[2]:  # Если такой же слот уже занят
+                        print("Слот занят")
+                        return True
+                value[2] = True  # Одеваем
+                print('Предмет {0} экипирован'.format(key))
+                self.effect_on(key, person)  # Добавляем эффекты
+                return True
         else:
             print('Такого предмета нет!')
+            return True
 
-    def effects(self, name):
+    def effect_on(self, name, person):
         match name:
             case 'Гномий щит':
-                pass
+                person.armor += 4
+            case 'Кулон смерти':
+                person.effects['Кулон смерти'] = True
+            case 'Кулон жизни':
+                person.max_hp += 75
+                person.restoration()
+
+    def effect_off(self, name, person):
+        match name:
+            case 'Гномий щит':
+                person.armor -= 4
+            case 'Кулон смерти':
+                del person.effects['Кулон смерти']
+            case 'Кулон жизни':
+                person.max_hp -= 75
+                person.restoration()
 
     def not_equipment(self):
         a = []
@@ -35,11 +56,13 @@ class Artefact():
                 a.append(value[3])
                 a.append(':')
                 a.append(key)
-                a.append('  ')
+                a.append(':')
+                a.append(value[0])
+                a.append(' ')
         if a:
             return a
         else:
-            return "[Рюкзак пуст]"
+            return "Рюкзак пуст"
 
     def equipment(self):
         a = []
@@ -48,9 +71,11 @@ class Artefact():
                 a.append(value[3])
                 a.append(':')
                 a.append(key)
-                a.append('  ')
+                a.append(':')
+                a.append(value[0])
+                a.append(' ')
         if a:
             return a
         else:
-            return "[Ничего не экипировано]"
+            return "Ничего не экипировано"
 
