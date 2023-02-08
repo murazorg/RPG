c9d3bb3a944f0fa6be58c5e9e66fde603bb7fd77
murazorg <123461932+murazorg@users.noreply.github.com>
2023-02-08 14:44:12 +0300

    Add files via upload

---
+++ enemy_system.py
@@ -0,0 +1,26 @@
+import random
+
+
+def random_enemy(person, difficulty):
+    enemy_lvl = {'Гоблин': [1, 2], 'Орк': [2, 4], 'Огр': [5, 8], 'Циклоп': [8, 13],
+                 'Малый энт': [1, 2], 'Гигантский паук': [2, 4], 'Сатир': [4, 8], 'Беорн': [7, 12], 'Большой энт': [9, 13]}
+    pull = []
+    match difficulty:
+        case 'easy':
+            for key, value in enemy_lvl.items():
+                if (person.lvl >= value[0]) and (person.lvl <= value[1]):
+                    pull.append(key)
+            print(pull)
+            return random.choice(pull)
+        case 'normal':
+            for key, value in enemy_lvl.items():
+                if (person.lvl >= (value[0] - 1)) and (person.lvl <= (value[1] - 1)):
+                    pull.append(key)
+            print(pull)
+            return random.choice(pull)
+        case 'hard':
+            for key, value in enemy_lvl.items():
+                if (person.lvl >= (value[0] - 2)) and (person.lvl <= (value[1] - 2)):
+                    pull.append(key)
+            print(pull)
+            return random.choice(pull)
